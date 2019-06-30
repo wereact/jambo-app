07c9963430804a0fc83e04da46f1efaf035b3f8a
Samuel Mataraso <samuelmataraso@gmail.com>
2019-06-30 20:31:28 -0300

    feat/ implementing NewsList.

---
+++ src/modules/news/components/Card.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components/native';
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+import { Metrics, Colors, Fonts } from '~/themes';
+
+const { size } = Metrics;
+const { fineBlack, white, fineGrey } = Colors;
+const { type, typography } = Fonts;
+
+const Container = styled.View.attrs(() => ({
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 1 },
+  shadowOpacity: 0.1,
+  shadowRadius: 2,
+  elevation: 1,
+}))`
+  display: flex;
+  flex: 1;
+  align-items: flex-start;
+  height: ${hp('20%')};
+  background: white;
+  border-radius: ${size(5)};
+  padding-left: ${wp('3%')};
+  padding-right: ${wp('3%')};
+  padding-top: ${hp('3%')};
+  padding-bottom: ${hp('3%')};
+  margin-left: ${wp('1%')};
+  margin-right: ${wp('1%')};
+`;
+
+const CategoryBox = styled.View`
+  height: ${hp('4%')};
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.color};
+  border-radius: ${size(5)};
+  padding-left: ${wp('3%')};
+  padding-right: ${wp('3%')};
+  margin-bottom: ${hp('2%')};
+`;
+
+const CategoryName = styled.Text.attrs(() => ({
+  ellipsizeMode: 'tail',
+  numberOfLines: 1,
+}))`
+  text-align: center;
+  font-family: ${type.sf.semiBold};
+  font-size: ${typography.small};
+  color: ${white};
+  font-weight: 500;
+`;
+
+const WrapperTitle = styled.View`
+  width: ${wp('70%')};
+  justify-content: center;
+  margin-bottom: ${hp('2%')};
+`;
+
+const TextTitle = styled.Text.attrs(() => ({
+  ellipsizeMode: 'tail',
+  numberOfLines: 2,
+}))`
+  text-align: left;
+  font-family: ${type.sf.regular};
+  font-size: ${typography.small};
+  color: ${fineBlack};
+  font-weight: 500;
+`;
+
+const WrapperDetails = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: ${hp('2%')};
+  width: ${wp('80%')};
+`;
+
+const WrapperDate = styled.View`
+  width: ${wp('20%')};
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const TextDate = styled.Text.attrs(() => ({
+  ellipsizeMode: 'tail',
+  numberOfLines: 1,
+}))`
+  text-align: center;
+  font-family: ${type.sf.medium};
+  font-size: ${typography.smallMinus};
+  color: ${fineGrey};
+  font-weight: normal;
+`;
+
+const WrapperSource = styled.View`
+  width: ${wp('30%')};
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const TextSource = styled.Text.attrs(() => ({
+  ellipsizeMode: 'tail',
+  numberOfLines: 1,
+}))`
+  text-align: center;
+  font-family: ${type.sf.medium};
+  font-size: ${typography.smallMinus};
+  color: ${fineGrey};
+  font-weight: normal;
+`;
+
+export default function Card(props) {
+  const { category, categoryColor, title, date, source } = props;
+
+  return (
+    <Container>
+      <CategoryBox color={categoryColor}>
+        <CategoryName>{category.toUpperCase()}</CategoryName>
+      </CategoryBox>
+      <WrapperTitle>
+        <TextTitle>{title}</TextTitle>
+      </WrapperTitle>
+      <WrapperDetails>
+        <WrapperDate>
+          <TextDate>{date}</TextDate>
+        </WrapperDate>
+        <WrapperSource>
+          <TextSource>{source}</TextSource>
+        </WrapperSource>
+      </WrapperDetails>
+    </Container>
+  );
+}
+
+Card.defaultProps = {
+  category: '',
+  categoryColor: white,
+  title: '',
+  date: '',
+  source: '',
+};
+
+Card.propTypes = {
+  category: PropTypes.string,
+  categoryColor: PropTypes.string,
+  title: PropTypes.string,
+  date: PropTypes.string,
+  source: PropTypes.string,
+};
--- src/modules/news/screens/NewsScreen.js
+++ src/modules/news/screens/NewsScreen.js
@@ -1,65 +1,129 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { FlatList } from 'react-native';
+// import PropTypes from 'prop-types';
 
 import styled from 'styled-components/native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
-import { StatusBarManager, Button } from '~/common/components';
-import { Metrics, Colors } from '~/themes';
+import { StatusBarManager, CollapsingToolbar } from '~/common/components';
+import { Card } from '~/modules/news/components';
 
-const { size, iPhoneXHelper } = Metrics;
-const { white } = Colors;
-
-const Container = styled.View`
+const Content = styled.View`
+  display: flex;
   flex: 1;
-  align-items: center;
-  background: ${white};
+  margin-left: ${wp('2%')};
+  margin-right: ${wp('2%')};
 `;
 
-const SafeArea = styled.SafeAreaView`
-  margin-top: ${iPhoneXHelper}px;
-  background: ${white};
+const Separator = styled.View`
+  height: ${hp('2%')};
 `;
-
-const WrapperButton = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: flex-end;
-  padding-bottom: ${size(50)}px;
-`;
-
-const ExampleText = styled.Text``;
 
 export function newsScreenConfig() {
-  //   return {
-  //     header: null,
-  //   };
+  return {
+    header: null,
+  };
 }
 
-const NewsScreen = props => {
-  const { navigation } = props;
-  const { navigate } = navigation;
+const NewsScreen = () => {
+  // const { navigation } = props;
+  // const { navigate } = navigation;
+
+  const data = [
+    {
+      id: '1',
+      category: 'BIM',
+      categoryColor: '#87CEFA',
+      title: 'Certificação de BIM',
+      date: '30/06/2019',
+      source: 'Folha da Engenharia',
+    },
+    {
+      id: '2',
+      category: 'Inovação',
+      categoryColor: '#FF8C00',
+      title: 'Jambo inovando o mercado de Curso de Engenharia Civl',
+      date: '10/06/2019',
+      source: 'Folha da Engenharia',
+    },
+    {
+      id: '3',
+      category: 'Engenharia',
+      categoryColor: '#228B22',
+      title: 'Certificação de BIM',
+      date: '05/03/2019',
+      source: 'Folha da Engenharia',
+    },
+    {
+      id: '4',
+      category: 'Tecnologia',
+      categoryColor: '#6A5ACD',
+      title: 'Jambo lança App voltado para Cursos em Engenharia Civil.',
+      date: '15/07/2019',
+      source: 'Folha da Engenharia',
+    },
+    {
+      id: '5',
+      category: 'BIM',
+      categoryColor: '#87CEFA',
+      title: 'BIM será obrigatório a partir de 2020',
+      date: '02/07/2019',
+      source: 'Folha da Engenharia',
+    },
+    {
+      id: '6',
+      category: 'Tecnologia',
+      categoryColor: '#6A5ACD',
+      title:
+        'Cursos da área de Engenharia Civil agora são feitos de modo Tecnologico/Online pela Jambo.',
+      date: '30/06/2019',
+      source: 'Folha da Engenharia',
+    },
+  ];
+
+  const renderNewsListItem = item => {
+    const { category, categoryColor, title, date, source } = item;
+
+    return (
+      <Card
+        category={category}
+        categoryColor={categoryColor}
+        title={title}
+        date={date}
+        source={source}
+      />
+    );
+  };
+
+  const renderItemSeparator = () => <Separator />;
+
+  const renderNewsList = () => (
+    <FlatList
+      data={data}
+      renderItem={({ item }) => renderNewsListItem(item)}
+      keyExtractor={item => item.id.toString()}
+      ItemSeparatorComponent={() => renderItemSeparator()}
+      ListFooterComponent={() => <Separator />}
+      ListHeaderComponent={() => <Separator />}
+      // extraData={this.state} // Flatlist re-render.
+    />
+  );
 
   return (
-    <Container>
-      <SafeArea>
-        <StatusBarManager />
-        <ExampleText>NewsScreen</ExampleText>
-        <WrapperButton>
-          <Button
-            labelText="Open"
-            variant="enable"
-            onPress={() => navigate('ProfileTest')}
-          />
-        </WrapperButton>
-      </SafeArea>
-    </Container>
+    <CollapsingToolbar headerTitle="Notícias">
+      <StatusBarManager />
+      <Content>{renderNewsList()}</Content>
+    </CollapsingToolbar>
   );
 };
 
 export default NewsScreen;
 
-NewsScreen.propTypes = {
-  navigation: PropTypes.objectOf(
-    PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  ).isRequired,
-};
+// NewsScreen.propTypes = {
+//   navigation: PropTypes.objectOf(
+//     PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+//   ).isRequired,
+// };
